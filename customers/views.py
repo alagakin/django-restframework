f19c5825989e3de4dced85e15e9ed30f2a532f74
Alexandr Agakin <agakinalexandr@gmail.com>
2023-04-22 23:21:39 +0200

    basic serializer

--- customers/views.py
+++ customers/views.py
@@ -2,17 +2,21 @@ from django.forms import model_to_dict
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from customers.models import Customer
+from customers.serializers import CustomerSerializer
 
 
 class CustomersApiView(APIView):
     def get(self, request):
-        lst = Customer.objects.all().values()
-        return Response({'posts': lst})
+        customers = Customer.objects.all()
+        return Response({'posts': CustomerSerializer(customers, many=True).data})
 
     def post(self, request):
+        serializer = CustomerSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
         post_new = Customer.objects.create(
             name=request.data['name'],
             content=request.data['content'],
             group_id=request.data['group_id']
         )
-        return Response({'post': model_to_dict(post_new)})
+        return Response({'post': CustomerSerializer(post_new).data})
