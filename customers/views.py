14c919548b4a06358938f29721e4279642d3aa98
Alexandr Agakin <agakinalexandr@gmail.com>
2023-04-22 22:27:28 +0200

    making post and get using basic APIView

--- customers/views.py
+++ customers/views.py
@@ -1,8 +1,18 @@
-from rest_framework import generics
+from django.forms import model_to_dict
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from customers.models import Customer
-from customers.serializers import CustomerSerializer
 
 
-class CustomersApiView(generics.ListAPIView):
-    queryset = Customer.objects.all()
-    serializer_class = CustomerSerializer
+class CustomersApiView(APIView):
+    def get(self, request):
+        lst = Customer.objects.all().values()
+        return Response({'posts': lst})
+
+    def post(self, request):
+        post_new = Customer.objects.create(
+            name=request.data['name'],
+            content=request.data['content'],
+            group_id=request.data['group_id']
+        )
+        return Response({'post': model_to_dict(post_new)})
