fc015f34117b9956490915b41a894935c65606ee
Alexandr Agakin <agakinalexandr@gmail.com>
2023-04-23 10:03:58 +0200

    RetrieveUpdateDestroyAPIView

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from customers.views import CustomersApiView, CustomersApiList
+from customers.views import  CustomersApiList, CustomerAPIDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/customers/', CustomersApiList.as_view()),
-    path('api/v1/customers/<int:pk>/', CustomersApiView.as_view())
-
+    path('api/v1/customer/<int:pk>/', CustomerAPIDetailView.as_view())
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,6 +1,4 @@
 from rest_framework import generics
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from customers.models import Customer
 from customers.serializers import CustomerSerializer
 
@@ -10,30 +8,6 @@ class CustomersApiList(generics.ListCreateAPIView):
     serializer_class = CustomerSerializer
 
 
-class CustomersApiView(APIView):
-    def put(self, request, *args, **kwargs):
-        pk = kwargs.get('pk', None)
-        if not pk:
-            return Response({'error': 'Method PUT is not allowed'})
-        try:
-            instance = Customer.objects.get(pk=pk)
-        except:
-            return Response({'error': 'Object does not exist'})
-
-        serializer = CustomerSerializer(data=request.data, instance=instance)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        return Response({'post': serializer.data})
-
-    def delete(self, request, *args, **kwargs):
-        pk = kwargs.get('pk', None)
-        if not pk:
-            return Response({'error': 'Method DELETE is not allowed'})
-
-        try:
-            Customer.objects.get(pk=pk).delete()
-        except:
-            return Response({'error': 'Object does not exist'})
-
-        return Response({'post': f"deleted customer with ID {pk}"})
+class CustomerAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
