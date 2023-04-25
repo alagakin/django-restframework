4a46b85487e893594e8356887198ecae6f0a2dbc
Alexandr Agakin <agakinalexandr@gmail.com>
2023-04-25 11:26:55 +0200

    custom pagination

--- customers/views.py
+++ customers/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, \
     IsAuthenticated
@@ -10,10 +11,16 @@ from customers.permissions import IsAdminOrReadOnly, IsOwnedOrReadOnly
 from customers.serializers import CustomerSerializer, GroupSerializer
 
 
+class CustomerAPIListPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 10
+
 class CustomerAPIList(generics.ListCreateAPIView):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
+    pagination_class = CustomerAPIListPagination
 
 
 class CustomerAPIUpdate(generics.RetrieveUpdateAPIView):
